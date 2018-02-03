432d5091a4249b6634ea55f778a82166b1a2a5a9
johnny <619147232@qq.com>
2018-02-03 18:14:27 +0800

    first commit

---
+++ src/test/java/com/tmo/tmo_base_jar/TmoBaseJarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tmo.tmo_base_jar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmoBaseJarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
